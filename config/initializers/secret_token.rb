9ad00180259843e565c0539b3dd56c6e834ec04d
Joseph Bell <jbell@numerex.com>
2012-04-13 08:34:33 -0500

    Shell

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoarExample::Application.config.secret_token = '1e650935814dc6be3c6771def5e9e748a9add5cdee35402a3744273d02f94a4b1ae5bb7222c7aa2ee4826590304c01c01efd1a8852df0b720bb778accca24803'
